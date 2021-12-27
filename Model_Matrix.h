92ec8f781a8ea6a3bcf5da855565961bae856e60
MiniXie_Student <55052123+Xie-Zehao@users.noreply.github.com>
2021-12-28 00:31:59 +0800

    添加View矩阵，并将多余代码移除

--- Model_Matrix.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include"Vector3.h"
-#include "Matrix.h"
-class Model_Matrix:public Matrix{
-public:
-	void ChangModelToWorld(float dx, float dy, float dz, float x, float y, float z, float sx, float sy, float sz);
-};
